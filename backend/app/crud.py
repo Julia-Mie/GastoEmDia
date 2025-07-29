6cdafb2a6bc8c46c9662142688e682f5db5bbd4e
Júlia Mie Suzuki <juliamie@usp.br>
2025-07-29 15:20:01 -0300

    front e back rodando

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,8 +1,10 @@
-from SQLAlchemy.orm import Session
+from sqlalchemy.orm import Session
 from app import models, schemas
+
 
 def get_items(db: Session):
     return db.query(models.Item).all()
+
 
 def create_item(db: Session, item: schemas.ItemCreate):
     db_item = models.Item(**item.dict())
@@ -10,3 +12,4 @@ def create_item(db: Session, item: schemas.ItemCreate):
     db.commit()
     db.refresh(db_item)
     return db_item
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from routers import items
-from database import engine, Base
+from app.routers import items
+from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(items.router)
